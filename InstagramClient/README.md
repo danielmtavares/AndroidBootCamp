5b23ac209c857d2af8c438016d605515a3f3c581
Daniel Tavares <dtavares@zynga.com>
2015-02-07 21:08:25 -0800

    Implement Instagram Client minimum requirements
    The following user stories have been implemented:
    
     - User can scroll through current popular photos
     - For each photo displayed, user can see the following details:
       - Graphic, caption and username

---
+++ InstagramClient/README.md
@@ -0,0 +1,25 @@
+Project 1: Instagram Client
+---------------------------
+
+**Overview:** Build a read-only photo viewer for Instagram which allows a user to check out popular photos.
+
+Required User Stories:
+----------------------
+
+- [x] User can scroll through current popular photos from Instagram
+
+For each photo displayed, user can see the following details:
+- [x] Graphic, Caption, Username
+- [] (Optional) relative timestamp, like count, user profile image
+
+
+Optional Advanced User Stories:
+-------------------------------
+
+- [] Advanced: Add pull-to-refresh for popular stream with SwipeRefreshLayout
+- [] Advanced: Show latest comment for each photo (bonus: show last 2 comments)
+- [] Advanced: Display each photo with the same style and proportions as the real Instagram (see screens below)
+- [] Advanced: Display each user profile image using a RoundedImageView
+- [] Advanced: Display a nice default placeholder graphic for each image during loading (read more about Picasso)
+- [] Advanced: Improve the user interface through styling and coloring
+- [] Bonus: Allow user to view all comments for an image within a separate screen or a dialog fragment
---
+++ InstagramClient/app/src/main/java/com/example/dtavares/instagramclient/InstagramPhotosAdapter.java
@@ -0,0 +1,48 @@
+package com.example.dtavares.instagramclient;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+import java.util.List;
+
+public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
+    // What data do we need from the activity
+    // Context, Data Source
+    public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
+        super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    // What our item looks like
+    // Use the template to display each photo
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        InstagramPhoto photo = getItem(position);
+
+        // Check if we are using a recycled view, if not we need to inflate
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+        }
+
+        // Lookup the views for populating the data (image, caption)
+        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+
+        // Insert the model data into each of the view items
+        tvCaption.setText(photo.username + ": " + photo.caption);
+
+        ivPhoto.setImageResource(0);
+        RequestCreator creator = Picasso.with(getContext()).load(photo.imageUrl);
+        creator.fit().centerCrop().placeholder(R.drawable.ic_launcher).into(ivPhoto);
+
+        return convertView;
+    }
+}
